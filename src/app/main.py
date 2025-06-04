8759929a1b2accbecda2642472aede700b3751f1
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-06-04 14:16:46 +0300

    api: Updated `Converter()`, `*.drawio.*`

--- src/app/main.py
+++ src/app/main.py
@@ -3,17 +3,35 @@ import io
 import pydub
 
 
-def converter(filepath: str) -> bytes:
-    audio_export = io.BytesIO()
+class Converter:
+    """Audio file converter
+    """
+    _filepath: str = ""
+    _export_file_format: str = ""
 
-    audio_segment = pydub.AudioSegment.from_wav(file=filepath)
-    audio_segment.export(audio_export, format="mp3")
+    def set_input(self, filepath: str):
+        self._filepath = filepath
 
-    return audio_export.getvalue()
+    def set_output(self, export_file_format: str = "mp3"):
+        self._export_file_format = export_file_format
+
+    def convert(self) -> bytes:
+        """Returns converted `wav` to `mp3` as bytes"""
+        audio_export = io.BytesIO()
+
+        audio_segment = pydub.AudioSegment.from_wav(
+            file=self._filepath
+        )
+        audio_segment.export(audio_export, format=self._export_file_format)
+
+        return audio_export.getvalue()
 
 
 if __name__ == "__main__":
-    output: bytes = converter(filepath="./samples/BAK.wav")
+    converter = Converter()
+
+    converter.set_input(filepath="./samples/BAK.wav")
+    converter.set_output(export_file_format="mp3")
 
     with open("./samples/BAK.mp3", "wb") as ftwb:
-        ftwb.write(output)
+        ftwb.write(converter.convert())
