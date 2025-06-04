99ba1dfa10cbc0c19f1ec5bd4982077fc434428f
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-06-04 18:54:20 +0300

    api_test: Updated `Converter`
    Added checks and Exceptions

--- src/app/main.py
+++ src/app/main.py
@@ -1,22 +1,66 @@
 import io
 
+import filetype
 import pydub
+
+
+class NonSequentialCallExp(Exception):
+    pass
+class UnsupportedFileTypeExp(Exception):
+    pass
+class UnsupportedExportFileFormatExp(Exception):
+    pass
 
 
 class Converter:
     """Audio file converter
     """
+
+    SUPPORTED_FILE_TYPES: tuple[str] = (
+        "audio/x-wav",
+    )
+    SUPPORTED_EXPORT_FILE_FORMAT: tuple[str] = (
+        "mp3",
+    )
+
     _filepath: str = ""
     _export_file_format: str = ""
 
     def set_input(self, filepath: str):
         self._filepath = filepath
+        kind = filetype.guess(self._filepath)
 
-    def set_output(self, export_file_format: str = "mp3"):
-        self._export_file_format = export_file_format
+        if not kind:
+            raise UnsupportedFileTypeExp(
+                "FileType is not supporeted, try:\n%s" % self.SUPPORTED_FILE_TYPES
+            )
+        if kind.mime not in self.SUPPORTED_FILE_TYPES:
+            raise UnsupportedFileTypeExp(
+                "FileType(`%s`) is not supporeted, try:\n%s" % (
+                    kidn.mime, self.SUPPORTED_FILE_TYPES
+                )
+            )
+
+    def set_output(self, export_file_format: str):
+        if not self._filepath:
+            raise NonSequentialCallExp("FilePath is not set, use: `set_input()`")
+
+        self._export_file_format: str = str(export_file_format).lower()
+
+        if self._export_file_format not in self.SUPPORTED_EXPORT_FILE_FORMAT:
+            raise UnsupportedExportFileFormatExp(
+                "FileFormat(`%s`) is not supported, use:\n%s" % (
+                    self._export_file_format, self.SUPPORTED_EXPORT_FILE_FORMAT,
+                )
+            )
 
     def convert(self) -> bytes:
         """Returns converted `wav` to `mp3` as bytes"""
+        if not self._filepath:
+            raise NonSequentialCallExp("FilePath is not set, use: `set_input()`")
+        if not self._export_file_format:
+            raise NonSequentialCallExp("FilePath is not set, use: `set_output()`")
+
         audio_export = io.BytesIO()
 
         audio_segment = pydub.AudioSegment.from_wav(
@@ -33,5 +77,7 @@ if __name__ == "__main__":
     converter.set_input(filepath="./samples/BAK.wav")
     converter.set_output(export_file_format="mp3")
 
+    converted_audio_file = converter.convert()
+
     with open("./samples/BAK.mp3", "wb") as ftwb:
-        ftwb.write(converter.convert())
+        ftwb.write(converted_audio_file)
