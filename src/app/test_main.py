6345587444d1d6ccd68d7b8eea01724cffa8fde2
YóUnǎi <c0d3r.nodiru.gaji@gmail.com>
2025-06-04 21:21:17 +0300

    api_test: Added `TestConverter()`

--- src/app/test_main.py
+++ src/app/test_main.py
@@ -1,36 +1,88 @@
 import pytest
 
+from main import (
+    NonSequentialCallExp,
+    UnsupportedFileTypeExp,
+    UnsupportedExportFileFormatExp
+)
 from main import Converter
 
 
-class TestClass:
+class TestConverter:
 
     @pytest.mark.parametrize(
-        ("filepath",),
+        ("filepath", "exception"),
         (
-            ("samples/BAK.wav",),
+            ("samples/WrongFilePath.wav", FileNotFoundError),
+            ("samples/BAK.mp3", UnsupportedFileTypeExp),
         )
     )
-    def test_input(self, filepath: str):
-        x = "this"
-        assert "h" in x
+    def test_input(
+        self,
+        filepath: str,
+        exception: Exception
+    ):
+        """Checks for file path and type
+
+        Supported file types are:
+            `SUPPORTED_FILE_TYPES`
+        """
+        converter = Converter()
+
+        with pytest.raises(exception):
+            converter.set_input(filepath=filepath)
 
     @pytest.mark.parametrize(
-        ("export_file_format",),
+        ("filepath", "export_file_format", "exception"),
         (
-            ("mp3",),
+            ("samples/BAK.wav", "mp3", NonSequentialCallExp),
+            ("samples/BAK.wav", "wav", UnsupportedExportFileFormatExp),
         )
     )
-    def test_ouput(self, export_file_format: str):
-        x = "this"
-        assert "h" in x
+    def test_ouput(
+        self,
+        filepath: str,
+        export_file_format: str,
+        exception: Exception
+    ):
+        """Checks for export file format
+
+        Supported export file formats are:
+            `SUPPORTED_EXPORT_FILE_FORMAT`
+        """
+        converter = Converter()
+
+        if exception is not NonSequentialCallExp:
+            converter.set_input(filepath=filepath)
+
+        with pytest.raises(exception):
+            converter.set_output(export_file_format=export_file_format)
 
     @pytest.mark.parametrize(
-        ("filepath", "export_file_format"),
+        ("filepath", "export_file_format", "exception", "task_id"),
         (
-            ("samples/BAK.wav", "mp3"),
+            ("samples/BAK.wav", "mp3", NonSequentialCallExp, 1),
+            ("samples/BAK.wav", "mp3", NonSequentialCallExp, 2),
         )
     )
-    def test_convert(self, filepath: str, export_file_format: str):
-        x = "this"
-        assert "h" in x
+    def test_convert(
+        self,
+        filepath: str,
+        export_file_format: str,
+        exception: Exception,
+        task_id: int
+    ):
+        """Checks for sequential function call
+
+        Correct SFC order:
+            `set_input()`
+            `set_output()`
+            `convert()`
+        """
+        converter = Converter()
+
+        if task_id != 1:
+            converter.set_input(filepath=filepath)
+
+        with pytest.raises(exception):
+            converter.convert()
